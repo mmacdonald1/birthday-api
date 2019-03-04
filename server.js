2a2eef452cbd311480aa9935f5cc48592ed39c85
mmacdonald1 <melmacd94@gmail.com>
2019-03-04 16:06:56 -0500

    server env

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // dependecies and global variables
 const express = require('express')
 const app =  express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.static(__dirname + '/public'))
 
