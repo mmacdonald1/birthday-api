b55c4f8769f9f478027705d9024575f8efa376e6
mmacdonald1 <melmacd94@gmail.com>
2019-03-05 12:44:26 -0500

    mock api

--- server.js
+++ server.js
@@ -2,11 +2,36 @@
 const express = require('express')
 const app =  express()
 const port = process.env.PORT || 3000
+const birthdays = [{
+  'id':1,
+  'name':'Kevin',
+  'phone':'222-222-2222',
+  'birthday':'03-11-1994'
+},
+{
+  'id':2,
+  'name':'Melanie',
+  'phone':'222-722-2222',
+  'birthday':'08-01-1994'
+},
+{
+  'id':3,
+  'name':'Chris',
+  'phone':'222-222-2422',
+  'birthday':'05-21-1994'
+},
+]
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req,res)=>{
-  res.sendFile(__dirname, + 'index.html')
+app.get('/birthdays', (req,res)=>{
+  res.json(birthdays)
+})
+
+app.get('/birthdays/:id', (req,res)=>{
+  let birthdayId =  parseInt(req.params.id)
+  let birthdayPerson = birthdays.filter(birthday => birthday.id === birthdayId)
+  birthdayPerson ? res.json(birthdayPerson) :  res.status(404).send()
 })
 
 app.listen(port, ()=>console.log(`Listening on port ${port}`))
