60c424b19bacbf58e65cfb80f2d3281827320525
eharris128 <echarris@smcm.edu>
2019-03-09 19:25:34 -0500

    Add CRUD endpoints for friends and users

--- server.js
+++ server.js
@@ -1,53 +1,98 @@
 require("dotenv").load();
+
 // dependecies and global variables
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const { Pool } = require("pg");
 const port = process.env.PORT || 3000;
-// data
-let nextBirthdayId = 3;
-let birthdays = [
-  {
-    id: 1,
-    name: "Kevin",
-    phone: "222-222-2222",
-    birthday: "03-11-1994"
-  },
-  {
-    id: 2,
-    name: "Melanie",
-    phone: "222-722-2222",
-    birthday: "08-01-1994"
-  },
-  {
-    id: 3,
-    name: "Chris",
-    phone: "222-222-2422",
-    birthday: "05-21-1994"
-  }
-];
+
+// db connection info
+const DB_URL = process.env.DB_URL;
+const DB_USER = process.env.DB_USER;
+const DB_HOST = process.env.DB_HOST;
+const DB_DATABASE = process.env.DB_DATABASE;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_PORT = process.env.DB_PORT;
+
+const pool = new Pool({
+  user: DB_USER,
+  host: DB_HOST,
+  database: DB_DATABASE,
+  password: DB_PASSWORD,
+  port: DB_PORT
+});
 
 // setting inputs to json and setting views directory shortcut
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "/public"));
 
-// GET all birthdays
-app.get("/birthdays", (req, res) => {
-  res.json(birthdays);
+// GET all friends
+app.get("/friends", (req, res) => {
+  pool.query("SELECT * FROM friends", (err, results) => {
+    check(err);
+    res.json(results.rows);
+  });
 });
 
-// Get a birthday by id
-app.get("/birthdays/:id", (req, res) => {
-  let birthdayId = parseInt(req.params.id);
-  let birthdayPerson = birthdays.filter(birthday => birthday.id === birthdayId);
-  birthdayPerson ? res.json(birthdayPerson) : res.status(404).send();
+// Get a friend by id
+app.get("/friends/:id", (req, res) => {
+  let friendId = parseInt(req.params.id);
+  pool.query(
+    "SELECT * FROM friends where id = $1",
+    [friendId],
+    (err, results) => {
+      check(err);
+      res.json(results.rows);
+    }
+  );
 });
 
-function isString(value) {
-  return typeof value === "string";
-}
-//Post a birthday
-app.post("/birthdays", (req, res) => {
+// Post a friend
+app.post("/friends", (req, res) => {
+  let body = req.body;
+  if (
+    !isString(body.name) ||
+    !isString(body.phone) ||
+    !isString(body.birthday) ||
+    !isNum(body.userId)
+  ) {
+    res.status(400).send();
+  }
+
+  pool.query(
+    "INSERT INTO friends (id, name, phone, birthday, user_id) VALUES (DEFAULT, $1, $2, $3, $4) RETURNING id",
+    // TODO: replace body.userId with user id from localstorage.
+    [body.name, body.phone, body.birthday, body.userId],
+    (err, results) => {
+      check(err);
+      let newPerson = {
+        id: results.rows[0].id,
+        name: body.name,
+        phone: body.phone,
+        birthday: body.birthday,
+        userId: body.userId
+      };
+      res.json(newPerson);
+    }
+  );
+});
+
+// Delete /friends/:id
+app.delete("/friends/:id", (req, res) => {
+  let friendId = parseInt(req.params.id);
+  pool.query(
+    "DELETE FROM friends where id = $1",
+    [friendId],
+    (err, results) => {
+      check(err);
+      res.json(req.params.id);
+    }
+  );
+});
+
+// Put /friends/:id
+app.put("/friends/:id", (req, res) => {
   let body = req.body;
   if (
     !isString(body.name) ||
@@ -56,37 +101,109 @@ app.post("/birthdays", (req, res) => {
   ) {
     res.status(400).send();
   }
-  body.id = nextBirthdayId++;
-  birthdays.push(body);
-  res.json(body);
+  let friendId = parseInt(req.params.id);
+  pool.query(
+    "UPDATE friends SET name = $1, phone = $2, birthday = $3 where id = $4 RETURNING id, user_id",
+    [body.name, body.phone, body.birthday, friendId],
+    (err, results) => {
+      check(err);
+      let newPerson = {
+        id: results.rows[0].id,
+        name: body.name,
+        phone: body.phone,
+        birthday: body.birthday,
+        userId: results.rows[0].user_id
+      };
+      res.json(newPerson);
+    }
+  );
 });
 
-// Delete /birthdays/:id
-app.delete("/birthdays/:id", (req, res) => {
-  let birthdayId = parseInt(req.params.id);
-  let birthdayPerson = birthdays.filter(birthday => birthday.id === birthdayId);
-  birthdays = birthdays.filter(birthday => birthday.id !== birthdayId);
-  console.log(birthdays);
+// user routes
 
-  birthdayPerson ? res.json(birthdayPerson) : res.status(404).send();
+// GET all users
+app.get("/users", (req, res) => {
+  pool.query("SELECT * FROM users", (err, results) => {
+    check(err);
+    res.json(results.rows);
+  });
 });
 
-// Put /birthdays/:id
-app.put("/birthdays/:id", (req, res) => {
-  let birthdayId = parseInt(req.params.id);
-  let birthdayPerson = birthdays.filter(birthday => birthday.id === birthdayId);
+// Get a user by id
+app.get("/users/:id", (req, res) => {
+  let userId = parseInt(req.params.id);
+  pool.query("SELECT * FROM users where id = $1", [userId], (err, results) => {
+    check(err);
+    res.json(results.rows);
+  });
+});
 
-  let indexToUpdate = birthdays.indexOf(birthdayPerson);
-  let updatedPerson = {
-    id: req.params.id,
-    name: req.body.name,
-    phone: req.body.phone,
-    birthday: req.body.birthday
-  };
+//Post a user
+app.post("/users", (req, res) => {
+  let body = req.body;
+  if (!isString(body.name) || !isString(body.phone)) {
+    res.status(400).send();
+  }
 
-  // manipulates birthdays. will not work with postgresql
-  birthdays.splice(indexToUpdate, 1, updatedPerson);
+  pool.query(
+    "INSERT INTO users (id, name, phone) VALUES (DEFAULT, $1, $2) RETURNING id",
+    [body.name, body.phone],
+    (err, results) => {
+      check(err);
+      let newUser = {
+        id: results.rows[0].id,
+        name: body.name,
+        phone: body.phone
+      };
+      res.json(newUser);
+    }
+  );
+});
+
+// Delete /users/:id
+app.delete("/users/:id", (req, res) => {
+  let userId = parseInt(req.params.id);
+  pool.query("DELETE FROM users where id = $1", [userId], (err, results) => {
+    check(err);
+    res.json(req.params.id);
+  });
+});
+
+// Put /users/:id
+app.put("/users/:id", (req, res) => {
+  let body = req.body;
+  if (!isString(body.name) || !isString(body.phone)) {
+    res.status(400).send();
+  }
+  let userId = parseInt(req.params.id);
+  pool.query(
+    "UPDATE users SET name = $1, phone = $2 where id = $3 RETURNING id",
+    [body.name, body.phone, userId],
+    (err, results) => {
+      check(err);
+      let newUser = {
+        id: results.rows[0].id,
+        name: body.name,
+        phone: body.phone
+      };
+      res.json(newUser);
+    }
+  );
 });
 
 //telling app which port to run on
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// helper funcs
+function check(err) {
+  if (err) {
+    throw err;
+  }
+}
+function isString(value) {
+  return typeof value === "string";
+}
+
+function isNum(value) {
+  return typeof value === "number";
+}
